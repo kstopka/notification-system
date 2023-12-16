12bd9823159e32c111d2dc27d55a6aa9d93d76c6
Krystian Stopka <kr6stopka@gmail.com>
2023-12-16 23:16:15 +0100

    del comments

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,10 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const checkToken = (req, res, next) => {
-  // Header names in Express are auto-converted to lowercase
   let token = req.headers["x-access-token"] || req.headers["authorization"];
 
-  // Remove Bearer from string
   token = token.replace(/^Bearer\s+/, "");
 
   if (token) {
