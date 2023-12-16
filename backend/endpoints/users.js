1fc471b14c20d0cba5a8438ba10d499cfbe6f7ce
Krystian Stopka <kr6stopka@gmail.com>
2023-12-16 23:06:28 +0100

    del comments

--- backend/endpoints/users.js
+++ backend/endpoints/users.js
@@ -12,21 +12,15 @@ class UsersClass {
     });
 
   checkPermissions = (request, response) => {
-    // Capture the input fields
     let email = request.body.email;
     let password = request.body.password;
-    // Ensure the input fields exists and are not empty
     if (email && password) {
-      // Execute SQL query that'll select the account from the database based on the specified email and password
       db.query(
         "SELECT * FROM users WHERE email = ? AND password = ?",
         [email, password],
         function (error, results, fields) {
-          // If there is an issue with the query, output the error
           if (error) throw error;
-          // If the account exists
           if (results.length > 0) {
-            // Authenticate the user
             request.session.loggedin = true;
             request.session.email = email;
 
