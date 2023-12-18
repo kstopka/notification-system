087d5ba1f4400f7a5a6c6d05dcc5647357ebe287
Krystian Stopka <kr6stopka@gmail.com>
2023-12-18 21:41:59 +0100

    fixes styles

--- frontend/src/components/News/SingleNews/styles.tsx
+++ frontend/src/components/News/SingleNews/styles.tsx
@@ -3,13 +3,26 @@ import styled from "styled-components";
 export const SingleNewsWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: ${({ theme }) => theme.colors.secondary};
-  color: ${({ theme }) => theme.colors.primary};
-  padding: 50px;
+  gap: 5px;
+
   width: 100%;
   max-width: 80%;
+  padding: 50px;
 
-  gap: 5px;
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export const ButtonsWrapper = styled.div<{ isActiveComment: boolean }>`
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+  justify-content: ${({ isActiveComment }) =>
+    isActiveComment ? "space-between" : "flex-end"};
+
+  @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.sm}) {
+    flex-direction: row;
+  }
 `;
 
 export const Title = styled.h5`
@@ -18,20 +31,17 @@ export const Title = styled.h5`
 
 export const Header = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
+  @media (min-width: ${({ theme: { breakpoints } }) => breakpoints.sm}) {
+    flex-direction: row;
+  }
 `;
 
 export const Content = styled.div`
   padding: 5px;
   min-height: 100px;
   border: solid 1px ${(props) => props.theme.colors.primary};
-`;
-
-export const ButtonsWrapper = styled.div<{ isActiveComment: boolean }>`
-  display: flex;
-
-  justify-content: ${({ isActiveComment }) =>
-    isActiveComment ? "space-between" : "flex-end"};
 `;
 
 export const ButtonsEndWrapper = styled.div`
